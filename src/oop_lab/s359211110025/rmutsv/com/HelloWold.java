2c6d1c9cdd59b201517e62c59f1e7285984bdb2a
montatip1995 <tan.montatip@gmail.com>
2017-11-19 09:49:07 +0700

    Testing upload to Git hub

---
+++ src/oop_lab/s359211110025/rmutsv/com/HelloWold.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110025.rmutsv.com;
+
+public class HelloWold
+{
+//    Create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello world,I'm Tan");
+    System.out.println("My name is Montatip Yodsawai");
+
+
+}//main
+
+
+}//class
